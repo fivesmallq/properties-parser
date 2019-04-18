174f9f201d6b3b5edaf1e97a4a048d43d36f0a25
fivesmallq <fivesmallq@gmail.com>
2019-04-18 10:39:35 +0800

    init project

---
+++ src/main/java/im/nll/data/config/SimpleProperties.java
@@ -0,0 +1,9 @@
+package im.nll.data.config;
+
+/**
+ * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
+ * @version Revision: 1.0
+ * @date 2019/4/18 10:38 AM
+ */
+public class SimpleProperties {
+}
