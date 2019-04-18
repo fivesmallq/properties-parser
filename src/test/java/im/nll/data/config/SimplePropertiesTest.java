8bdcee5100be2f75c0bbd2598cfe7132c97cf334
fivesmallq <fivesmallq@gmail.com>
2019-04-18 14:14:05 +0800

    base code

--- src/test/java/im/nll/data/config/SimplePropertiesTest.java
+++ src/test/java/im/nll/data/config/SimplePropertiesTest.java
@@ -1,9 +1,19 @@
 package im.nll.data.config;
 
+import org.junit.Test;
+
+import java.util.Properties;
+
 /**
  * @author <a href="mailto:fivesmallq@gmail.com">fivesmallq</a>
  * @version Revision: 1.0
  * @date 2019/4/18 10:38 AM
  */
 public class SimplePropertiesTest {
+    @Test
+    public void readConfTest() {
+        SimpleProperties simpleProperties = SimpleProperties.readConfiguration("redis.properties");
+        Properties configuration = simpleProperties.getUnderlyingProperties();
+        System.out.println(configuration);
+    }
 }
